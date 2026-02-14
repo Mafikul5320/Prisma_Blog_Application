409eee3c8cabb742b0ca570edc7e2028530db977
Mafikul Islam <mdmafikulislam5320@gmail.com>
2026-02-14 23:24:15 +0600

    set up better auth

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import express, { Application } from 'express'
 import { PostRouter } from './modules/Post/post.route'
+import { toNodeHandler } from "better-auth/node";
+import { auth } from './lib/auth';
 
 const app: Application = express()
 app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use("/post", PostRouter)
 
 export default app;
